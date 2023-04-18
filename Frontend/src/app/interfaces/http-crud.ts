d0b4f1966dc7a1d031ff39fd9800c2a126bc3cd2
Ivan0707z <bloodyline0904@gmail.com>
2023-04-18 14:06:21 +0300

    -

--- Frontend/src/app/interfaces/http-crud.ts
+++
@@ -1,6 +0,0 @@
-import { Observable } from "rxjs";
-import { ITanks } from "./i-tanks";
-
-export interface HttpCrudInterface {
-    getdata():Observable<ITanks>;
-}
--- lab-6/Backend/src/main/java/com/example/Backend/repository/DeviceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Backend.repository;
-
-import com.example.Backend.entity.DeviceEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeviceRepository extends JpaRepository<DeviceEntity, Long> {
-}
